d0faaca1f1b172d5b65a1d3823dea29402cddedc
Elizabeth Zevin <ehzevin@gmail.com>
2019-02-11 00:49:42 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -6,38 +6,3 @@ def using_unshift(array, string)
   array.unshift(string)
 end 
 
-def using_pop(array)
-  array.pop
-end
-
-def pop_with_args(array)
-  array.pop(2)
-end 
-
-def using_shift(array)
-  array.shift
-end 
-
-def shift_with_args(array)
-  array.shift(2)
-end
-
-def using_concat(array, array)
-end
- 
-def using_insert(array, element)
-  array.insert(element)
-end
-  
-def using_uniq(array)
-end
-
-def using_flatten(array)
-end
-  
-def using_delete (array, string)
- array.delete(string)  
-end
- 
-def using_delete_at (array, integer)
-end
